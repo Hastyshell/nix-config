4a75e2619fbc05c521dd5a03b76976162fdba9b5
Hastyshell <tangsiyang2001@gmail.com>
2026-06-04 00:42:04 +0800

    fix(home): load nix profile for dev server zsh

--- hosts/home/hasty-dev-server/default.nix
+++ hosts/home/hasty-dev-server/default.nix
@@ -8,5 +8,12 @@
   system = "x86_64-linux";
   homeModules = [
     self.homeModules.default
+    {
+      programs.zsh.envExtra = ''
+        if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
+          . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+        fi
+      '';
+    }
   ];
 }
