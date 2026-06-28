5839b4549101c9f2e588ce58e4d29636d4dcf542
Hastyshell <tangsiyang2001@gmail.com>
2026-06-28 17:53:11 +0800

    fix(maccy): fix launch command so Maccy restarts normally

--- modules/home/darwin/desktop/autostart.nix
+++ modules/home/darwin/desktop/autostart.nix
@@ -2,23 +2,21 @@
 let
   homeManagerApps = "${config.home.homeDirectory}/${config.targets.darwin.copyApps.directory}";
 
-  openAppPath = appPath: {
+  openAppPathWithArgs = openArgs: appPath: {
     enable = true;
     config = {
-      ProgramArguments = [
-        "/usr/bin/open"
-        "-gj"
-        appPath
-      ];
+      ProgramArguments = [ "/usr/bin/open" ] ++ openArgs ++ [ appPath ];
       RunAtLoad = true;
     };
   };
+  openAppPath = openAppPathWithArgs [ "-gj" ];
   openHomeManagerApp = name: openAppPath "${homeManagerApps}/${name}.app";
+  openForegroundHomeManagerApp = name: openAppPathWithArgs [ ] "${homeManagerApps}/${name}.app";
 in
 {
   launchd.agents = {
     clash-verge = openAppPath "/Applications/Clash Verge.app";
-    maccy = openHomeManagerApp "Maccy";
+    maccy = openForegroundHomeManagerApp "Maccy";
     stats = openHomeManagerApp "Stats";
     tailscale = openAppPath "/Applications/Tailscale.app";
   };
